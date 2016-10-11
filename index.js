59541c5628ab60f610b9baf85c2ab4eaeb4a48b8
Alexander Mills <alexander.mills@autodesk.com>
2016-10-11 12:41:05 -0700

    temp

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node --harmony
+
+process.on('uncaughtException', function (e) {
+    console.log(e.stack || e);
+});
+
+process.on('unhandledRejected', function (e) {
+    console.log(e.stack || e);
+});
+
+process.on('exit', function (code) {
+    console.log(' => caGor is exiting...with code => ' + code);
+    console.log('\n\n');
+});
+
+require('./lib/clone-all-repos');
